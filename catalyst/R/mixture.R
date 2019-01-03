02007eff8c75953bcf463fa28a5f0c176f71de44
shannong19 <sgallagh@andrew.cmu.edu>
2019-01-03 18:49:44 -0500

    one bug squashed.  next step is variance for EM.  yay

--- catalyst/R/mixture.R
+++ catalyst/R/mixture.R
@@ -17,7 +17,9 @@ gen_mix_2_data <- function(w, p1, p2,
                            x0){
     ## Get initial times
     mat <- matrix(0, nrow = T, ncol = N)
-    mat[1, 1:x0] <- 1
+    if(x0 > 0){
+        mat[1, 1:x0] <- 1
+    }
     ## Latent variable Z
     Z <- rbinom(n = N, size = 1, prob = w) # 1 is group 1, 0 is group 2
 
@@ -41,71 +43,119 @@ gen_mix_2_data <- function(w, p1, p2,
 
 #' Use EM alg. to get estimates of two group bernoulli mixture model
 #'
-#' @param theta0 vector of (w, p1, p2) - initial values
+#' @param theta_init vector of (w, p_vec) - initial values
 #' @param agent_data TxN matrix of agent data where entry t,n is state of agent n at time t
 #' @param tol float - our convergence tolerance.  Default is .001
 #' @param max_it integer - max number of iterations.  Default is 1000
 #' @return list of best theta values (w, p1, p2) and conv_it iteration on which alg. converged or 0 if it did not
-EM_mix_2 <- function(theta0, agent_data,
-                     tol=1e-5, max_it = 1e3){
-    theta <- theta0
-
-    ## Subset agent_data to get rid of agents infected from beginning - they do not contribute to likelihood
+EM_mix_2 <- function(theta_init, agent_data,
+                     tol=1e-3, max_it = 1e3){
+    theta <- theta_init
+  #  print(theta)
+    ## Subset agent_data to get rid of agents infected from beginning - they do not contribute to likelihood for the SI model
     init_inf_inds <- which(agent_data[1,] == 1)
-    agent_data <- agent_data[ ,-init_inf_inds]
+    if(length(init_inf_inds) > 0){
+        agent_data <- agent_data[ ,-init_inf_inds]
+    }
 
     for(ii in 1:max_it){
 
+
         theta_new <- EM_calc_mix2(theta, agent_data)
         if(sqrt(sum((theta - theta_new)^2)) <= tol){
-            return(list(theta = theta, conv_it = ii))
+            return(list(theta = theta, conv_it = ii,
+                        theta_init = theta_init))
         }
         theta <- theta_new
-
+#        print(theta)
 
 
     }
 
-    return(list(theta = theta_new, conv_it = 0))
+    return(list(theta = theta_new, conv_it = 0,
+                theta_init = theta_init))
 
 }
 
 #' Do inner calculations for EM
 #'
-#' @param theta vector (w, p1, p2)
+#' @param theta vector (w, p_vec)
 #' @param agent_data agent_data TxN matrix of agent data where entry t,n is state of agent n at time t
 #' @return updated theta
 #' @details see dissertation for calculations
 EM_calc_mix2 <- function(theta, agent_data){
-    T <- nrow(agent_data)
-    theta_new <- theta
-    w <- theta[1]
-    p1 <- theta[2]
-    p2 <- theta[3]
-    sum_atn <- apply(agent_data[-T, , drop = FALSE],
-                     2, function(col) sum(col == 0)) # vecof length N
-    becomes_inf <- apply(agent_data, 2, function(col){
-        if(any(col == 1)){
-            return(1)
-        } else {
-            return(0)
-        }
-    })
-    EZ_cond <- get_cond_exp_mix2(w, p1, p2, agent_data) # vectof of length N
-    ## New values
-    ## p1
-    denom1 <- sum(EZ_cond * sum_atn)
-    theta_new[2] <- sum(EZ_cond * becomes_inf) / denom1
-    ## p2
-    denom2 <- sum((1- EZ_cond) * sum_atn)
-    theta_new[3] <- sum((1-EZ_cond) * becomes_inf) / denom2
-    ## w
-    theta_new[1] <- denom1 / (denom1 + denom2)
-    return(theta_new)
+    K <- length(theta) / 2
+    w <- theta[1:K]
+    p_vec <- theta[-c(1:K)]
 
+    ## E Step
+    EZ_cond <- E_step_SI(w, p_vec, agent_data)
+
+    ## M Step
+    new_theta <- M_step_SI(EZ_cond, agent_data)
+    return(new_theta)
     
 
 
+}
+
+#' Do M step for SI mixture model
+#'
+#' @param EZ_cond expected value of Z conditioned on w and p_vec - a KxN matrix where entry k n is the expected value of Z for agent n.
+#' @param  agent_data  agent_data TxN matrix of agent data where entry t,n is state of agent n at time t.  All agents are initially susceptible as the initially infected have been extracted
+#' @return updated w_vec and updated p_vec combined into theta c(w_vec, p_vec)
+M_step_SI <- function(EZ_cond, agent_data){
+    K <- nrow(EZ_cond)
+    T <- nrow(agent_data)
+    ## Get the number of days each agent was susceptible
+    days_sus <- apply(agent_data[-T, , drop = FALSE], 2, function(col){
+        sum(col == 0)
+    })
+    became_inf <- apply(agent_data, 2, function(col){
+        sum(sum(col == 1) > 0) # at least one 1, which means agent became infected, zero otherwise
+    })
+    N_vec <- rowSums(EZ_cond * matrix(rep(days_sus, each = K), nrow = K)) # effective observations
+    ## w
+    w_vec <- N_vec / sum(N_vec)
+    ##
+    num <- rowSums(EZ_cond * matrix(rep(became_inf, each = K), nrow = K)) # numerator for updated p
+    p_vec <- num / N_vec
+    return(c(p_vec, w_vec))
+    
+
+}
+
+#' Calculate the E Step for SI mixture model
+#'
+#' @param w vector of length K, weights for different mixtures
+#' @param p_vec vector of length K, prob of becoming infected given individual is in group K
+#' @param agent_data  agent_data TxN matrix of agent data where entry t,n is state of agent n at time t.  All agents are initially susceptible as the initially infected have been extracted
+#' @return expected value of Z conditioned on w and p_vec - a KxN matrix where entry k n is the expected value of Z for agent n.  
+E_step_SI <- function(w, p_vec, agent_data){
+
+    K <- length(w)
+    N <- ncol(agent_data)
+    T <- nrow(agent_data)
+    log_num_mat <- matrix(0, nrow = K, ncol = N)
+    num_mat <- log_num_mat
+    Z_mat <- num_mat
+    for(kk in 1:K){
+        for(nn in 1:N){
+            log_num_mat[kk, nn] <-  log(w[kk])
+            for(tt in 1:(T-1)){
+                current_state <- agent_data[tt, nn]
+                if(current_state == 1){
+                    break
+                }
+                next_state <- agent_data[tt+1, nn]
+                log_num_mat[kk, nn] <- log_num_mat[kk, nn] + next_state * log(p_vec[kk]) +
+                    (1-next_state) * log(1 - p_vec[kk])
+            }
+            num_mat <- exp(log_num_mat)
+        }
+    }
+    Z_mat <- num_mat / matrix(rep(colSums(num_mat), each = K), nrow = K)
+    return(Z_mat)
 }
 
 
--- catalyst/tests/testthat/test-mixture.R
+++ catalyst/tests/testthat/test-mixture.R
@@ -16,32 +16,58 @@ test_that("generating the data",{
                                  x0)$agent_data
     expect_true(all(unlist(agent_data[-1,]) == 1))
 
-###
-    w <- 0
-    p1 <- 0
-    p2 <- 1
-    agent_data <- gen_mix_2_data(w, p1, p2,
-                                 N, T,
-                                 x0)$agent_data
-    expect_true(all(unlist(agent_data[-1,]) == 1))
-###
-                                        #    set.seed(2019)
-    N <- 10000
+    
+### EZ_step_SI
+    N <- 10
     T <- 2
-    x0 <- 1
+    x0 <- 0
     w <- .5
-    p1 <- .6
-    p2 <- .1
+    p1 <- .1
+    p2 <- .8
+    ##
     agent_data_list <- gen_mix_2_data(w, p1, p2,
                                  N, T,
                                  x0)
-    agent_data_list$Z
     agent_data <- agent_data_list$agent_data
-    agent_data
+    Z <- agent_data_list$Z
+    theta <- c(.5, .5, .4) #c(w, p_vec)
+
+
+    p_vec <- c(p1, p2)
+    w_vec <- c(w, 1 - w)
+    EZ_cond <- E_step_SI(w_vec, p_vec, agent_data)
+    expect_true(all(colSums(EZ_cond) == 1))
+
+
+    ## M step following EZ
+    out <- M_step_SI(EZ_cond, agent_data)
+
+
+    ###########################################
+    ## do itt
+    N <- 1000
+    T <- 8
+    x0 <- 0
+    w <- .5
+    p1 <- .1
+    p2 <- .8
+    K <-  2
+    ##
+    agent_data_list <- gen_mix_2_data(w, p1, p2,
+                                 N, T,
+                                 x0)
+    agent_data <- agent_data_list$agent_data
+    Z <- agent_data_list$Z
+    theta <- c(.5, .5, .4) #c(w, p_vec)
+
+
+    p_vec <- c(p1, p2)
+    w_vec <- c(w, 1 - w)
+    r <- runif(2)
+    theta_init <- c(r/ sum(r), rep(1/K, K))
     devtools::load_all()
-    ## Try it out
-    theta0 <- c(.6, .5, .1)
-    out <- EM_mix_2(theta0, agent_data)
-    out
+    best_params <- EM_mix_2(theta_init, agent_data, max_it = 1000)
+    best_params
 
 })
+
